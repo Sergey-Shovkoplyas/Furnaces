5981e9865dd8b95f73865171c75e64a223359f1f
Sergey Shovkoplyas <shovkoplyassergey@gmail.com>
2019-12-24 20:48:57 +0200

    Header in progress

--- app/js/common.js
+++ app/js/common.js
@@ -1,11 +1,27 @@
-window.onload = function () {
-	
-	console.log('js is working!!!'); 
-
-}
-
-/* $( document ).ready(function() {
+$( document ).ready(function() {
 
 	console.log( 'JQuery!!!' );
 
-}); */
+	$('.furnaces__slider').slick({
+		dots: true
+	});
+
+	$('.header__burger').on('click', function(e) {
+		$('.header__burger, .header__nav').toggleClass('active');
+		$('body').toggleClass('locked');
+		if ( !$('.header').hasClass('active') ) {
+			$('.header').addClass('active');
+		}
+	});
+
+	$(document).on('scroll', function(e) {
+		let top = $('html').scrollTop();
+		if(top > 0) {
+			$('.header').addClass('active');
+		} else {
+			$('.header').removeClass('active');
+		}
+		
+	});
+
+});
