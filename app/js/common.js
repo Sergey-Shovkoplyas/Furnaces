5f413916ab4495c574e5f0b159c4a838f8ea0567
Sergey Shovkoplyas <shovkoplyassergey@gmail.com>
2020-01-02 19:21:47 +0200

    Added section services

--- app/js/common.js
+++ app/js/common.js
@@ -1,10 +1,26 @@
 $( document ).ready(function() {
 
-	console.log( 'JQuery!!!' );
-
 	$('.furnaces__slider').slick({
 		dots: true
 	});
+	
+	$(window).on('resize orientationchange load', function() {
+		let $slider = $('.services__slider');
+		if ( $(window).width() >= 768 ) {
+			console.log('Desctop!!!');
+			if ( $slider.hasClass('slick-initialized') ) {
+				$slider.slick('unslick');
+			}
+		} else {
+			console.log('Mobile!!!');
+			if ( !$slider.hasClass('slick-initialized') ) {
+				$slider.slick({
+					dots: false
+				});
+			}
+		}
+	});
+
 
 	$('.header__burger').on('click', function(e) {
 		$('.header__burger, .header__nav').toggleClass('active');
